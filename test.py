823344a94e78d48dbd6a19c1fad37439a5e35416
joe_doe <damascanion@gmail.com>
2026-07-11 21:58:39 +0300

    new line of code

--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 print("Hello, New Branch gg")
-=======
-print("Hello, New Branch fix error bugg")
-print("hello the bag was fixed merge this")
->>>>>>> emergency-fix
+print ('new line')
